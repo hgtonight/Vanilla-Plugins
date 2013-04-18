04cf0a3cc8e13a6b68a9b96c644caa2774353e96
Zachary Doll <Zachary.Doll@gmail.com>
2013-04-18 16:01:01 -0500

    Updated style to work with Embed Friendly out of the box. Added check to not display module if embed vanilla is enabled.

--- JumpToTop/class.jumptotop.plugin.php
+++ JumpToTop/class.jumptotop.plugin.php
@@ -17,7 +17,7 @@
 $PluginInfo['JumpToTop'] = array(
 	'Name' => 'Jump To Top',
 	'Description' => 'A simple Vanilla Forums plugin that provides a link to jump to the top of the page. A special thanks to vrijvlinder for the idea.',
-	'Version' => '1.0',
+	'Version' => '1.1',
 	'RequiredApplications' => array('Vanilla' => '2.0.10'),
 	'Author' => "Zachary Doll",
 	'AuthorEmail' => 'hgtonight@gmail.com',
@@ -35,7 +35,7 @@ class JumpToTop extends Gdn_Plugin {
 	//This is a common hook that fires for all controllers on the Render method
 	public function Base_Render_Before($Sender) {
 		// bring in the module into every controller that isn't in the dashboard view
-		if($Sender->MasterView != 'admin') {
+		if($Sender->MasterView != 'admin' && !C('EnabledPlugins.embedvanilla',FALSE)) {
 			include_once(PATH_PLUGINS.DS.'JumpToTop'.DS.'class.jumptotop.module.php');
 			$Module = new JumpToTopModule($Sender);
 			$Sender->AddModule($Module);
