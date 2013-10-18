3f821810f366899fee3a7989264eb12ab093a7cb
Zachary Doll <Zachary.Doll@gmail.com>
2013-10-18 11:10:24 -0500

    Add a nonsensical locale item

--- BulkEdit/locale/en-CA/definitions.php
+++ BulkEdit/locale/en-CA/definitions.php
@@ -1,4 +1,4 @@
 <?php if (!defined('APPLICATION')) exit();
 /* Copyright 2013 Zachary Doll */
 // Add any plugin specific locale definitions here to make life easier for translators
-$Definition['Plugins.TestingGround.SadTruth'] = 'I do not do anything yet :(';
+$Definition['Bulk Edit'] = 'Bulk Edit';
